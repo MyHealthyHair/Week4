c8c1c8cf8513df14372915b5a6a675b4883d1529
MyHealthyHair <55239423+MyHealthyHair@users.noreply.github.com>
2019-09-23 14:49:22 +0800

    Add files via upload

---
+++ age.py
@@ -0,0 +1,13 @@
+age=20
+if age<2:
+	print('he is a baby')
+elif age<4:
+	print('he is learing to walk')
+elif age<13:
+	print('he is a child')
+elif age<20:
+	print('he is a teenager')
+elif age<65:
+	print('he is an adult')
+elif age>=65:
+	print('he is an old man')
---
+++ alien_color.py
@@ -0,0 +1,50 @@
+alien_color=['green']
+if 'green' in alien_color:
+ print('you got 5 points!')
+ 
+alien_color=['red']
+if 'green' in alien_color:
+ print('you got 5 points!')
+ 
+print('\n')
+
+alien_color=['green']
+if 'green' in alien_color:
+    print('you got 5 points!')
+else:
+	print('you got 10 points!')
+	
+print('\n')
+
+alien_color=['green']
+if 'red' in alien_color:
+    print('you got 5 points!')
+else:
+	print('you got 10 points!')
+	
+print('\n')
+	
+alien_color = 'green'
+if alien_color == 'green':
+	print('you got 5 points!')
+elif alien_color == 'yellow':
+	print('you got 10 points!')
+elif alien_color=='red':
+	print('you got 15 points!')
+
+alien_color = 'yellow'
+if alien_color == 'green':
+	print('you got 5 points!')
+elif alien_color == 'yellow':
+	print('you got 10 points!')
+elif alien_color=='red':
+	print('you got 15 points!')
+	
+alien_color = 'red'
+if alien_color == 'green':
+	print('you got 5 points!')
+elif alien_color == 'yellow':
+	print('you got 10 points!')
+elif alien_color=='red':
+	print('you got 15 points!')
+
